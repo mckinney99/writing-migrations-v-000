ce3504b6dab0000bef3fa33337f2cd54a4758d6b
Eric McKinney <mckinney99@gmail.com>
2017-12-08 18:19:17 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 3) do
   create_table "students", force: :cascade do |t|
     t.string  "name"
     t.integer "grade"
-    t.string  "birthdate"
+    t.datetime  "birthdate"
   end
 
 end
